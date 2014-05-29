b91d3b26b09c833b6f9e4b03389ccafce5810c6a
J. Anthony Sterrett, Jr <jasterrett@gmail.com>
2014-05-29 11:57:32 -0700

    Added colors

---
+++ src/Color.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+struct Color {
+  Color() :
+    Color( 0, 0, 0, 0 ) {}
+
+  Color( uint8_t _r, uint8_t _g, uint8_t _b, uint8_t _a ) :
+    red( _r ), green( _g ), blue( _b ), alpha( _a ) {}
+
+  uint8_t red, green, blue, alpha;
+};
+
+namespace colors {
+  const Color WHITE      {   0,   0,   0,   0 };
+  const Color RED        { 255,   0,   0,   0 };
+  const Color BLUE       {   0,   0, 255,   0 };
+  const Color GRAY       { 100, 100, 100,   0 };
+  const Color LIGHT_RED  { 127,   0,   0,   0 };
+  const Color LIGHT_BLUE {   0,   0, 127,   0 };
+  const Color LIGHT_GRAY {  50,  50,  50,   0 };
+  const Color BLACK      { 255, 255, 255,   0 };
+}
